8596d3b898666bb51b330f6cd2592b37fe0e6490
Roman Boytsov <domovon@outlook.com>
2023-05-24 21:24:58 +0300

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public double calculate (int credit, double percent, int months) {
+    public double calculate(int credit, double percent, int months) {
         double interestRate = percent / 12 / 100; //Процентная ставка
         double result;
-        result = credit * interestRate * Math.pow((1 + interestRate), months)/((Math.pow((1 + interestRate), months))-1);
+        result = credit * interestRate * Math.pow((1 + interestRate), months) / ((Math.pow((1 + interestRate), months)) - 1);
         return (int) result;
     }
 }
